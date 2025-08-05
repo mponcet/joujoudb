1e472b8f5f1e3295fdfd9faa9b45c110ca48f099
Maxime Poncet <maxime.poncet@gmail.com>
2025-08-05 10:59:24 +0200

    helper for btree leaf page split

--- src/pages/btree.rs
+++ src/pages/btree.rs
@@ -3,7 +3,7 @@ use crate::pages::{PAGE_SIZE, PageId};
 
 use zerocopy_derive::*;
 
-const BTREE_BRANCHING_FACTOR: usize = 340;
+const BTREE_BRANCHING_FACTOR: usize = 341;
 const BTREE_NUM_KEYS: usize = BTREE_BRANCHING_FACTOR - 1;
 
 enum BTreePageType {
@@ -32,98 +32,126 @@ impl BTreePageHeader {
     }
 }
 
-type Key = u32;
+pub type Key = u32;
 
-#[derive(Copy, Clone, Default, FromBytes, KnownLayout, Immutable)]
+#[derive(Copy, Clone, FromBytes, KnownLayout, Immutable)]
 #[repr(C)]
-struct RecordId {
+pub struct RecordId {
     page_id: PageId,
     slot_id: HeapPageSlotId,
 }
 
 #[derive(FromBytes, KnownLayout, Immutable)]
 #[repr(C)]
-struct BTreePage {
+pub struct BTreeLeafPage {
     header: BTreePageHeader,
     keys: [Key; BTREE_NUM_KEYS],
-    values: [RecordId; BTREE_BRANCHING_FACTOR],
+    values: [RecordId; BTREE_NUM_KEYS],
     prev: PageId,
     next: PageId,
-    _padding: [u8; 8],
+    _padding: [u8; 4],
 }
 
-const _: () = assert!(std::mem::size_of::<BTreePage>() == PAGE_SIZE);
+const _: () = assert!(std::mem::size_of::<BTreeLeafPage>() == PAGE_SIZE);
 
-impl Default for BTreePage {
+#[cfg(test)]
+impl Default for BTreeLeafPage {
     fn default() -> Self {
         Self {
             header: BTreePageHeader::default(),
             keys: [Key::default(); BTREE_NUM_KEYS],
-            values: [RecordId::default(); BTREE_BRANCHING_FACTOR],
+            values: [RecordId {
+                page_id: 0,
+                slot_id: 0,
+            }; BTREE_NUM_KEYS],
             prev: Default::default(),
             next: Default::default(),
-            _padding: [0; 8],
+            _padding: [0; 4],
         }
     }
 }
 
 use thiserror::Error;
 #[derive(Error, Debug)]
-enum BTreePageError {
+pub enum BTreePageError {
     #[error("key not found")]
     KeyNotFound,
-    #[error("page is full, a split is needed")]
-    SplitNeeded,
 }
 
-impl BTreePage {
+pub struct SplitLeaf<'page> {
+    lhs: &'page mut BTreeLeafPage,
+}
+
+impl SplitLeaf<'_> {
+    pub fn split(&mut self, rhs: &mut BTreeLeafPage, key: Key, value: RecordId) {
+        let lhs_num_keys = self.lhs.header.num_keys as usize;
+        let split_at = lhs_num_keys.div_ceil(2);
+        let rhs_num_keys = lhs_num_keys - split_at;
+        let median_key = self.lhs.keys[split_at];
+        rhs.keys[..rhs_num_keys].copy_from_slice(&self.lhs.keys[split_at..]);
+        rhs.values[..rhs_num_keys].copy_from_slice(&self.lhs.values[split_at..]);
+
+        self.lhs.header.num_keys = split_at as u16;
+        rhs.header.num_keys = rhs_num_keys as u16;
+
+        if key < median_key {
+            self.lhs.insert(key, value);
+        } else {
+            rhs.insert(key, value);
+        }
+    }
+}
+
+impl BTreeLeafPage {
     #[inline]
     fn keys(&self) -> &[Key] {
         let num_keys = self.header.num_keys as usize;
         &self.keys[..num_keys]
     }
 
-    fn search(&self, key: Key) -> Result<RecordId, BTreePageError> {
-        let idx = self
-            .keys()
-            .binary_search(&key)
-            .map_err(|_| BTreePageError::KeyNotFound)?;
-
-        Ok(self.values[idx])
+    #[inline]
+    fn values(&self) -> &[RecordId] {
+        let num_keys = self.header.num_keys as usize;
+        &self.values[..num_keys]
     }
 
-    fn insert(&mut self, key: Key, value: RecordId) -> Result<(), BTreePageError> {
+    pub fn search(&self, key: Key) -> Option<RecordId> {
+        let pos = self.keys().binary_search(&key).ok()?;
+        Some(self.values[pos])
+    }
+
+    pub fn insert(&mut self, key: Key, value: RecordId) -> Option<SplitLeaf<'_>> {
         match self.keys().binary_search(&key) {
-            Ok(idx) => {
+            Ok(pos) => {
                 // FIXME: key exists, replace value ?
-                self.values[idx] = value;
-                Ok(())
+                self.values[pos] = value;
+                None
             }
-            Err(idx) => {
+            Err(pos) => {
                 let num_keys = self.header.num_keys as usize;
-                if num_keys <= BTREE_NUM_KEYS {
-                    self.keys.copy_within(idx..num_keys, idx + 1);
-                    self.keys[idx] = key;
-                    self.values.copy_within(idx..num_keys, idx + 1);
-                    self.values[idx] = value;
+                if num_keys < BTREE_NUM_KEYS {
+                    self.keys.copy_within(pos..num_keys, pos + 1);
+                    self.keys[pos] = key;
+                    self.values.copy_within(pos..num_keys, pos + 1);
+                    self.values[pos] = value;
                     self.header.num_keys += 1;
-                    Ok(())
+                    None
                 } else {
-                    Err(BTreePageError::SplitNeeded)
+                    Some(SplitLeaf { lhs: self })
                 }
             }
         }
     }
 
-    fn delete(&mut self, key: Key) -> Result<(), BTreePageError> {
+    pub fn delete(&mut self, key: Key) -> Result<(), BTreePageError> {
         let num_keys = self.header.num_keys as usize;
-        let idx = self
+        let pos = self
             .keys()
             .binary_search(&key)
             .map_err(|_| BTreePageError::KeyNotFound)?;
 
-        self.keys.copy_within(idx + 1..num_keys, idx);
-        self.values.copy_within(idx + 1..num_keys, idx);
+        self.keys.copy_within(pos + 1..num_keys, pos);
+        self.values.copy_within(pos + 1..num_keys, pos);
         self.header.num_keys -= 1;
 
         Ok(())
@@ -134,36 +162,60 @@ impl BTreePage {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_basic_operations() {
-        let mut leaf = BTreePage::default();
-        for key in 0..BTREE_NUM_KEYS {
-            let _ = leaf.insert(key as u32, RecordId::default());
+    fn make_record(key: Key) -> RecordId {
+        RecordId {
+            page_id: key as PageId,
+            slot_id: key as HeapPageSlotId,
         }
-        assert_eq!(leaf.header.num_keys, BTREE_NUM_KEYS as u16);
-        assert!(leaf.keys().is_sorted());
-
-        let _ = leaf.delete((BTREE_NUM_KEYS / 2) as u32);
-        assert_eq!(leaf.header.num_keys, (BTREE_NUM_KEYS - 1) as u16);
-        assert_eq!(
-            leaf.keys[BTREE_NUM_KEYS / 2],
-            (BTREE_NUM_KEYS / 2 + 1) as u32
-        );
-        assert!(leaf.keys().is_sorted());
-
-        assert!(leaf.search(0).is_ok());
-        let _ = leaf.delete(0);
-        assert!(leaf.search(0).is_err());
     }
 
     #[test]
-    fn test_insert_no_order() {
-        let mut leaf = BTreePage::default();
+    fn test_leaf_page_basic() {
+        let mut leaf = BTreeLeafPage::default();
         for key in 0..BTREE_NUM_KEYS {
-            let key = (if key % 2 == 0 { key } else { key * 2 }) as u32;
-            let _ = leaf.insert(key, RecordId::default());
+            let _ = leaf.insert(key as Key, make_record(key as Key));
+        }
+        assert_eq!(leaf.keys().len(), BTREE_NUM_KEYS);
+        assert!(leaf.keys().is_sorted());
+
+        let key = (BTREE_NUM_KEYS / 2) as Key;
+        assert!(leaf.search(key).is_some());
+        let _ = leaf.delete(key);
+        assert!(leaf.search(key).is_none());
+        assert!(leaf.keys().is_sorted());
+    }
+
+    #[test]
+    fn test_insert_leaf_page_not_monotonic() {
+        let mut leaf = BTreeLeafPage::default();
+        for key in 0..BTREE_NUM_KEYS {
+            let key = (if key % 2 == 0 { key } else { key * 2 }) as Key;
+            let _ = leaf.insert(key as Key, make_record(key as Key));
         }
 
         assert!(leaf.keys().is_sorted());
+    }
+
+    #[test]
+    fn test_split_leaf_page() {
+        let mut lhs = BTreeLeafPage::default();
+        let mut rhs = BTreeLeafPage::default();
+
+        // fill lhs
+        for key in 0..BTREE_NUM_KEYS {
+            let key = key * 2;
+            lhs.insert(key as Key, make_record(key as Key));
+        }
+
+        // lhs is full, split needed
+        let key = BTREE_NUM_KEYS - BTREE_NUM_KEYS % 2 + 1;
+        let (key, value) = (key as Key, make_record(key as Key));
+        let split = lhs.insert(key, value);
+        assert!(split.is_some());
+        split.unwrap().split(&mut rhs, key, value);
+
+        assert!(lhs.keys().is_sorted());
+        assert!(rhs.keys().is_sorted());
+        assert_eq!(lhs.keys().len() + rhs.keys().len(), BTREE_NUM_KEYS + 1);
     }
 }
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -2,5 +2,6 @@ mod btree;
 mod heappage;
 mod page;
 
+pub use btree::{BTreeLeafPage, Key, RecordId};
 pub use heappage::{HeapPage, HeapPageSlotId};
 pub use page::{PAGE_SIZE, Page, PageId, PageMetadata};
