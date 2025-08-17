83165b6413baf551de8c69a5c2135caec33f73d5
Maxime Poncet <maxime.poncet@gmail.com>
2025-08-18 00:03:11 +0200

    btree: optimize insert with optimistic locking

--- benches/btree_contention.rs
+++ benches/btree_contention.rs
@@ -2,18 +2,37 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
 fn btree_contention_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("btree insert contention benchmark");
-    group.bench_function("2 threads", |b| {
-        b.iter(|| btree_lock_benchmark_call(black_box(2)));
+    let mut group = c.benchmark_group("btree mixed contention benchmark - slow path");
+    group.sample_size(10);
+    group.bench_function("9 readers , 1 writer", |b| {
+        b.iter(|| btree_mixed_benchmark_call::<false>(black_box(9)));
     });
+    group.finish();
+
+    let mut group = c.benchmark_group("btree mixed contention benchmark - fast path");
+    group.sample_size(10);
+    group.bench_function("9 readers , 1 writer", |b| {
+        b.iter(|| btree_mixed_benchmark_call::<true>(black_box(9)));
+    });
+    group.finish();
+
+    let mut group = c.benchmark_group("btree insert contention benchmark - slow path");
+    group.sample_size(10);
     group.bench_function("8 threads", |b| {
-        b.iter(|| btree_lock_benchmark_call(black_box(8)));
+        b.iter(|| btree_write_benchmark_call::<false>(black_box(8)));
     });
     group.bench_function("16 threads", |b| {
-        b.iter(|| btree_lock_benchmark_call(black_box(16)));
+        b.iter(|| btree_write_benchmark_call::<false>(black_box(16)));
     });
-    group.bench_function("32 threads", |b| {
-        b.iter(|| btree_lock_benchmark_call(black_box(32)));
+    group.finish();
+
+    let mut group = c.benchmark_group("btree insert contention benchmark - fast path");
+    group.sample_size(10);
+    group.bench_function("8 threads", |b| {
+        b.iter(|| btree_write_benchmark_call::<true>(black_box(8)));
+    });
+    group.bench_function("16 threads", |b| {
+        b.iter(|| btree_write_benchmark_call::<true>(black_box(16)));
     });
     group.finish();
 }
@@ -22,12 +41,61 @@ extern crate joujoudb;
 use joujoudb::indexes::BTree;
 use joujoudb::pages::{Key, RecordId};
 use joujoudb::storage::Storage;
+
 use std::sync::Arc;
 use std::thread;
 
-fn btree_lock_benchmark_call(num_threads: usize) {
-    let storage_path = format!("/tmp/btree_contention_test_{}.db", uuid::Uuid::new_v4());
-    let storage = Storage::open(&storage_path).unwrap();
+use tempfile::NamedTempFile;
+
+fn btree_mixed_benchmark_call<const FAST_PATH: bool>(num_read_threads: usize) {
+    let storage_path = NamedTempFile::new().unwrap();
+    let storage = Storage::open(storage_path).unwrap();
+
+    let btree = Arc::new(BTree::try_new(storage).unwrap());
+    let mut threads = Vec::new();
+    let btree_clone = Arc::clone(&btree);
+    let start_key = 0;
+    let end_key = 16000;
+
+    for _ in 0..num_read_threads {
+        let btree_clone = Arc::clone(&btree);
+
+        let handle = thread::spawn(move || {
+            for key in start_key..end_key {
+                let _ = btree_clone.search(key as Key);
+            }
+        });
+
+        threads.push(handle);
+    }
+
+    let handle = thread::spawn(move || {
+        // HACK: stop when reader threads stop
+        while Arc::strong_count(&btree_clone) > 2 {
+            for key in start_key..end_key {
+                let record_id = RecordId::new(0, 0);
+
+                if FAST_PATH {
+                    btree_clone.insert(key as Key, record_id).unwrap();
+                } else {
+                    btree_clone.insert_slow_path(key as Key, record_id).unwrap();
+                }
+            }
+            for key in start_key..end_key {
+                btree_clone.delete(key as Key).unwrap();
+            }
+        }
+    });
+    threads.push(handle);
+
+    for handle in threads {
+        handle.join().unwrap();
+    }
+}
+
+fn btree_write_benchmark_call<const FAST_PATH: bool>(num_threads: usize) {
+    let storage_path = NamedTempFile::new().unwrap();
+    let storage = Storage::open(storage_path).unwrap();
 
     let btree = Arc::new(BTree::try_new(storage).unwrap());
 
@@ -44,7 +112,11 @@ fn btree_lock_benchmark_call(num_threads: usize) {
         let handle = thread::spawn(move || {
             for key in start_key..end_key {
                 let record_id = RecordId::new(0, 0);
-                btree_clone.insert(key as Key, record_id).unwrap();
+                if FAST_PATH {
+                    btree_clone.insert(key as Key, record_id).unwrap();
+                } else {
+                    btree_clone.insert_slow_path(key as Key, record_id).unwrap();
+                }
             }
         });
 
