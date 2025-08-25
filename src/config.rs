158f8fedddd5534bcaa844c0f84f77ac39e510e4
Maxime Poncet <maxime.poncet@gmail.com>
2025-08-25 15:27:34 +0200

    storage: open or create new file

--- src/config.rs
+++ src/config.rs
@@ -6,8 +6,11 @@ use std::sync::LazyLock;
 pub struct Config {
     // number of pages in cache
     pub PAGE_CACHE_SIZE: usize,
+    // root directory
+    pub ROOT_DIRECTORY: String,
 }
 
 pub static CONFIG: LazyLock<Config> = LazyLock::new(|| Config {
     PAGE_CACHE_SIZE: DEFAULT_PAGE_CACHE_SIZE,
+    ROOT_DIRECTORY: "/tmp/".to_string(),
 });
